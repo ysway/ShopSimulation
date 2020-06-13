9ff06e07d2f66eb71b42895055a8727fc69871c5
kwan574-k <kwan574@aucklanduni.ac.nz>
2020-06-13 19:50:12 +1200

    Add image and name for phones
    Add image and update the corresponding id and name for 30 different phones

--- app/src/main/java/com/Group11android/PhoneProvider.java
+++ app/src/main/java/com/Group11android/PhoneProvider.java
@@ -5,17 +5,22 @@ import java.util.Random;
 
 public class PhoneProvider {
 
-    static String[] phmks = {"Phone0", "Phone1", "Phone2", "Phone3", "Phone4", "Phone5", "Phone6", "Phone7",
-            "Phone8", "Phone9"};
+    static String[] phmks = {"Huawei P40 pro", "Huawei p40", "Huawei p40 pro+", "Huawei Mate Xs",
+            "Huawei Mate30 pro", "Huawei Y6s", "Huawei P30", "Huawei P30 pro",
+            "Huawei Nova 5T", "Huawei Y9 Prime","Galaxy A01","Galaxy S20","Galaxy A31", "Galaxy A71",
+            "Galaxy Z Flip","Galaxy A80","Galaxy A51","Galaxy Fold","Galaxy A90","Galaxy Note 10",
+            "OPPO FindX2 pro","OPPO FindX2 Lite","OPPO FindX",
+            "OPPO Reno2","OPPO Reno2z","OPPO Reno10x","OPPO A91","OPPO A9","OPPO AX7","OPPO AX5s"};
 
-    static String[] ids = {"001", "002", "003", "004", "005", "006", "007", "008",
-            "009", "010"};
+    static String[] ids = {"H01", "H02", "H03", "H04", "H06", "H07", "H08", "H09", "H10", "S01",
+            "S02","S03","S04","S05","S06","S07","S08","S09","S10","O01","O02","O03","O04","O05",
+            "O06","O07","O08","O09","O10"};
 
-    static int[] coveraddrs = {R.drawable.b001, R.drawable.b002, R.drawable.b003, R.drawable.b004,R.drawable.b005,R.drawable.b006,R.drawable.b007,
-            R.drawable.b008,R.drawable.b009,R.drawable.b010};
+    static int[] coveraddrs = {R.drawable.h1, R.drawable.h2, R.drawable.h3, R.drawable.h4,R.drawable.h5,
+            R.drawable.h6,R.drawable.h7,R.drawable.h8,R.drawable.h9,R.drawable.h10};
 
 
-    public static ArrayList<Phone> generateData() {
+    public static ArrayList<Phoneh1> generateData() {
         ArrayList<Phone> phones = new ArrayList<Phone>();
 
 
